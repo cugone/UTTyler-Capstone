871616025226578508a3ab319758ce6dc070da70
Casey Ugone <cugone@gmail.com>
2016-03-30 17:39:49 +0000

    Added servo_test.py, updated softshut.py

---
+++ MLB_Box/servo_test.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+import math
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+
+pulsesPerCycle = 60
+pwm = GPIO.PWM(11, pulsesPerCycle)
+
+pi_list = [x for x in range(0, 2)]
+print(pi_list)
+
+for i in pi_list:
+    print(i * 0.06)
+    pwm.start(i * 0.06)
+    time.sleep(0.16)
+#end for
+
+pwm.stop()
+GPIO.cleanup()
+
+print("DONE! :D")
